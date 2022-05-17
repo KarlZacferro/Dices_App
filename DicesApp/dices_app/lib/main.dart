8a2ce20de5a8b03025897ac6eb214b4751be9dd6
Your Name <kbb25t@gmail.com>
2022-05-17 20:13:38 -0300

    Modificações

--- DicesApp/dices_app/lib/main.dart
+++ DicesApp/dices_app/lib/main.dart
@@ -1,26 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dices_app.dart';
 
-void main(){
-  runApp(MeuApp());
-}
-
-class MeuApp extends StatelessWidget{
-  const MeuApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 77, 167, 81),
-        body: DicesGame(),
-        appBar: AppBar(
-          title: Text(''),
-          ),
-          ),
-
-    );
-    throw UnimplementedError();
-  }
-
-  
-} 
+void main() => runApp(
+  const DicesApp());
